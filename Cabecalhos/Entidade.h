c4fd7a1e125eaacd42ea90a3e5fcfa0242d74124
Ricardo Reyes <rreyes@alunos.utfpr.edu.br>
2023-03-27 10:25:06 -0300

    Mudanças
    Mudei o nome do diagrama, atualizei o gitignore(agora ele não vai incluir arquivos que são próprios de cada máquina). E mexi na Entidade

--- Cabecalhos/Entidade.h
+++ Cabecalhos/Entidade.h
@@ -6,15 +6,22 @@ namespace Entidades {
 	class Entidade :
 		public Ente {
 	protected:
-		sf::Vector2i posicao;
-
+		sf::Vector2f posicao, velocidade, aceleracao;
+		int massa;
+		static const int Gravidade;
 	public:
 		Entidade();
-		Entidade(sf::Vector2i posi);
+		Entidade(sf::Vector2f posi);
 		~Entidade();
 
 		virtual void executar() = 0;
+		void Aplicar_Gravidade();
+		void Aplicar_Forca(sf::Vector2f forca);
+		sf::Vector2f Calc_Aceleracao(sf::Vector2f Forca);
+		sf::Vector2f Calc_Velocidade(sf::Vector2f Aceleracao);
+		sf::Vector2f Calc_Posicao(sf::Vector2f Velocidade);
+		sf::Vector2f Limitar_Velocidade(sf::Vector2f Velocidade);
 	};
 
 
-	}
+}
--- Fontes/Entidade.cpp
+++ Fontes/Entidade.cpp
@@ -1,11 +1,14 @@
 #include "../Cabecalhos/Entidade.h"
 
-Entidades::Entidade::Entidade():Ente() {
+Entidades::Entidade::Entidade():Ente(),massa(5) {
 }
 
-Entidades::Entidade::Entidade(sf::Vector2i posi){
+Entidades::Entidade::Entidade(sf::Vector2f posi):massa(5) {
 	posicao = posi;
 }
 
 Entidades::Entidade::~Entidade() {
 }
+
+
+const int Entidades::Entidade::Gravidade = 10;
