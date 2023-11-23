8868c2e81cf842b9a9b5a47771da6cf405bd5a4f
Cris2021UT <112227932+crisfrota-menezes@users.noreply.github.com>
2023-11-22 23:23:30 -0300

    Update Floresta.cpp
    Comentários para ficar mais facil pro ricardo entender funcionamento

--- Fontes/Floresta.cpp
+++ Fontes/Floresta.cpp
@@ -77,6 +77,7 @@ void Fases::Floresta::CriarMoscas() {
 }
 
 void Fases::Floresta::CriarGosmas() {
+	// abre arquivo para leitura
 	std::ifstream arquivo(ARQUIVO);
 	if (!arquivo)
 	{
@@ -86,6 +87,7 @@ void Fases::Floresta::CriarGosmas() {
 
 	// verifica se o arquivo esta vazio:
 	if (arquivo.peek() == -1) {
+		// caso esteja faz a criação padrão após fechar o arquivo
 		arquivo.close();
 		for (int i = 0; i < num_Gosmas; i++) {
 			Gosma* pGosma = new Gosma(); pGosma->setPosi(pos_Gosmas[i] * 16, altura_spawn_inimigos);
@@ -96,24 +98,34 @@ void Fases::Floresta::CriarGosmas() {
 	}
 	else
 	{
+		// caso não esteja vazio, decodifica o arquivo com o parse
 		nlohmann::json json = nlohmann::json::parse(arquivo);
-
+		
+		// cria aux
 		Gosma* pGosma;
 
+		// percorre o arquivo
 		for (auto it = json.begin(); it != json.end(); ++it) {
 			// pega o id da entidade e converte para string para comparar com o id da Gosma
 			string id = to_string((*it).front());
+			// compara o id lido do arquivo com o da gosma
 			if (id == "[3]") {
+				// cria auxiliar para armazenar a posição e velocidade da Gosma
 				sf::Vector2f pos = sf::Vector2f(
 					(float)((*it)["posicao"][0]),
 					(float)((*it)["posicao"][1])
 				);
 				float vel = (float)((*it)["velocidade"][0]);
+				// cria a Gosma com os valores lidos do arquivo e insere na lista de entidades
 				LEs.InserirEntidade(static_cast<Entidade*> (pGosma = new Gosma(pos, vel)));
+				// seta posicao que foi recuperada
 				pGosma->setPosi(pos);
+				// adiciona no gerenciado de colisoes
 				G_Colisoes.addInimigo(static_cast<Inimigo*>(pGosma));
+				// incrementa o numero de inimigos
 				num_inimigos++;
 			}
+			// limpa o id para não dar problema na proxima iteração
 			id = "";
 		}
 	}
